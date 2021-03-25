a3d4f5e800af73515862605edd84679fc583331f
Nicola Murino <nicola.murino@gmail.com>
2021-03-25 19:31:21 +0100

    improve signals handling

--- service/sighup_windows.go
+++
@@ -1,3 +0,0 @@
-package service
-
-func registerSigHup() {}
---
+++ service/signals_windows.go
@@ -0,0 +1,19 @@
+package service
+
+import (
+	"os"
+	"os/signal"
+
+	"github.com/drakkan/sftpgo/logger"
+)
+
+func registerSignals() {
+	c := make(chan os.Signal, 1)
+	signal.Notify(c, os.Interrupt)
+	go func() {
+		for range c {
+			logger.Debug(logSender, "", "Received interrupt request")
+			os.Exit(0)
+		}
+	}()
+}
