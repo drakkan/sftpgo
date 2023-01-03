0190d0b849a8c24faa841d7600e8ab18f7d59590
Nicola Murino <nicola.murino@gmail.com>
2023-01-03 10:18:30 +0100

    update Copyright year
    Signed-off-by: Nicola Murino <nicola.murino@gmail.com>

--- internal/metric/metric.go
+++ internal/metric/metric.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2019-2022  Nicola Murino
+// Copyright (C) 2019-2023 Nicola Murino
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published
@@ -10,7 +10,7 @@
 // GNU Affero General Public License for more details.
 //
 // You should have received a copy of the GNU Affero General Public License
-// along with this program.  If not, see <https://www.gnu.org/licenses/>.
+// along with this program. If not, see <https://www.gnu.org/licenses/>.
 
 //go:build !nometrics
 // +build !nometrics
