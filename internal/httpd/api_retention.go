784b7585c199f313927cb9a386ca23f0d96d9908
Nicola Murino <nicola.murino@gmail.com>
2024-01-01 11:31:45 +0100

    remove end year from Copyright notice in files
    so we don't have to update all the files every year
    
    Signed-off-by: Nicola Murino <nicola.murino@gmail.com>

--- internal/httpd/api_retention.go
+++ internal/httpd/api_retention.go
@@ -1,4 +1,4 @@
-// Copyright (C) 2019-2023 Nicola Murino
+// Copyright (C) 2019 Nicola Murino
 //
 // This program is free software: you can redistribute it and/or modify
 // it under the terms of the GNU Affero General Public License as published
