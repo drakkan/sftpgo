501a870e9da05daaa16f43f8eb5fe8ee1fc7cdba
Nicola Murino <nicola.murino@gmail.com>
2024-05-15 10:37:44 +0200

    docker: download plugins compatible with SFTPGo 2.5.x
    Signed-off-by: Nicola Murino <nicola.murino@gmail.com>

--- docker/scripts/download-plugins.sh
+++ docker/scripts/download-plugins.sh
@@ -17,9 +17,23 @@ esac
 
 echo "download plugins for arch ${SUFFIX}"
 
-for PLUGIN in geoipfilter kms pubsub eventstore eventsearch metadata auth
-do
-    echo "download plugin from https://github.com/sftpgo/sftpgo-plugin-${PLUGIN}/releases/latest/download/sftpgo-plugin-${PLUGIN}-linux-${SUFFIX}"
-    curl -L "https://github.com/sftpgo/sftpgo-plugin-${PLUGIN}/releases/latest/download/sftpgo-plugin-${PLUGIN}-linux-${SUFFIX}" --output "/usr/local/bin/sftpgo-plugin-${PLUGIN}"
-    chmod 755 "/usr/local/bin/sftpgo-plugin-${PLUGIN}"
-done
+curl -L "https://github.com/sftpgo/sftpgo-plugin-geoipfilter/releases/download/v1.0.7/sftpgo-plugin-geoipfilter-linux-${SUFFIX}" --output "/usr/local/bin/sftpgo-plugin-geoipfilter"
+chmod 755 "/usr/local/bin/sftpgo-plugin-geoipfilter"
+
+curl -L "https://github.com/sftpgo/sftpgo-plugin-kms/releases/download/v1.0.10/sftpgo-plugin-kms-linux-${SUFFIX}" --output "/usr/local/bin/sftpgo-plugin-kms"
+chmod 755 "/usr/local/bin/sftpgo-plugin-kms"
+
+curl -L "https://github.com/sftpgo/sftpgo-plugin-pubsub/releases/download/v1.0.11/sftpgo-plugin-pubsub-linux-${SUFFIX}" --output "/usr/local/bin/sftpgo-plugin-pubsub"
+chmod 755 "/usr/local/bin/sftpgo-plugin-pubsub"
+
+curl -L "https://github.com/sftpgo/sftpgo-plugin-eventstore/releases/download/v1.0.15/sftpgo-plugin-eventstore-linux-${SUFFIX}" --output "/usr/local/bin/sftpgo-plugin-eventstore"
+chmod 755 "/usr/local/bin/sftpgo-plugin-eventstore"
+
+curl -L "https://github.com/sftpgo/sftpgo-plugin-eventsearch/releases/download/v1.0.15/sftpgo-plugin-eventsearch-linux-${SUFFIX}" --output "/usr/local/bin/sftpgo-plugin-eventsearch"
+chmod 755 "/usr/local/bin/sftpgo-plugin-eventsearch"
+
+curl -L "https://github.com/sftpgo/sftpgo-plugin-metadata/releases/download/v1.0.12/sftpgo-plugin-metadata-linux-${SUFFIX}" --output "/usr/local/bin/sftpgo-plugin-metadata"
+chmod 755 "/usr/local/bin/sftpgo-plugin-metadata"
+
+curl -L "https://github.com/sftpgo/sftpgo-plugin-auth/releases/download/v1.0.5/sftpgo-plugin-auth-linux-${SUFFIX}" --output "/usr/local/bin/sftpgo-plugin-auth"
+chmod 755 "/usr/local/bin/sftpgo-plugin-auth"
