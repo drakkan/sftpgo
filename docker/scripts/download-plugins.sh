0be7545d9bce0502ee703c959dfa7f0504c76618
Nicola Murino <nicola.murino@gmail.com>
2025-12-29 08:55:46 +0100

    docker: remove trailing whitespace from download script
    Signed-off-by: Nicola Murino <nicola.murino@gmail.com>

--- docker/scripts/download-plugins.sh
+++ docker/scripts/download-plugins.sh
@@ -22,7 +22,7 @@ PLUGINS=(geoipfilter kms pubsub eventstore eventsearch auth)
 for PLUGIN in "${PLUGINS[@]}"; do
     URL="https://github.com/sftpgo/sftpgo-plugin-${PLUGIN}/releases/latest/download/sftpgo-plugin-${PLUGIN}-linux-${SUFFIX}"
     DEST="/usr/local/bin/sftpgo-plugin-${PLUGIN}"
-    
+
     echo "Downloading ${PLUGIN}..."
     if curl --fail --silent --show-error -L "${URL}" --output "${DEST}"; then
         chmod 755 "${DEST}"
@@ -33,4 +33,3 @@ for PLUGIN in "${PLUGINS[@]}"; do
 done
 
 echo "All plugins downloaded successfully"
-
